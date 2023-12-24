7c07199e628ff2e0357c8cf171352e58f6550138
Jake Delaney <jakedelaney24@gmail.com>
2023-12-24 01:29:18 +0000

    Initial commit

---
+++ 3-changing_html_css.js
@@ -0,0 +1,35 @@
+/**
+ * Modify HTML content and CSS styling using jQuery methods
+ * To change CSS styling, best practice is use .addClass() method to HTML elements
+ */
+
+//retrieves HTML from class "table-start"
+$(".table-start").html()
+
+//add paragraph to all h1 elements
+$("h1").html("<p>Hello world!</p>")
+
+//change styling through adding and removing class to/from HTML element
+$(".btn1".addClass("btn-green"))
+$(".btn1".removeClass("btn-green"))
+
+//hide or show elements
+$("#hide").click(function(){
+    $("p").hide();
+  });
+  
+  $("#show").click(function(){
+    $("p").show();
+  });
+
+// Modify the text content of all of the paragraph elements on the page
+$("p").text("New text");
+
+// Append a span to every paragraph
+$("p").append("<span>lorem ipsum</span>");
+
+// Remove all links using the remove function
+$("a").remove();
+
+// Empty all div elements that have a class of card
+$("div.card").empty();
